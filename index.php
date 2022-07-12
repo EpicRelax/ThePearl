6905ac9ebca5927d4fc27ac3f213c21ece59e322
chris1496587456321 <zsb_chris@hotmail.com>
2022-07-12 13:32:11 +0200

    second commit

--- index.php
+++ index.php
@@ -23,17 +23,14 @@
                     <a href="./index.php">
                         <li>HOME</li>
                     </a>
-                    <a href="">
-                        <li>SUITES</li>
-                    </a>
-                    <a href="">
-                        <li>ACTIVITIES</li>
-                    </a>
                     <a href="./assets/lib/Reservation/reservation.php">
                         <li>RESÉRVATION</li>
                     </a>
                     <a href="./assets/lib/Contact/contact.php">
                         <li>CONTACT</li>
+                    </a>
+                    <a href="./assets/lib/Contact/contact.php">
+                        <li>CONNECTION</li>
                     </a>
                 </ul>
             </nav>
@@ -47,73 +44,69 @@
         </div>
     </header>
     <main>
-        <h2>Our Services</h2>
+
+        <h2>Our Hotel</h2>
         <div id="service">
             <div id="room">
                 <img src="./assets/img/hotels-luxe-15-maroc.png" alt="">
                 <div id="description">
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. At ut vero, id perferendis animi et
-                        enim corrupti, debitis praesentium explicabo atque ex odit? Atque, excepturi. Laudantium
-                        commodi, fugit repudiandae veritatis id provident reprehenderit iusto hic iste blanditiis rem
-                        minima atque asperiores, voluptate fuga similique qui? Quos ut quis numquam dolorem optio minima
-                        quaerat, libero cumque. Cum, repellat. Incidunt eius expedita, animi quisquam ipsam, inventore
-                        tenetur, magni labore possimus repellendus nobis rem illo provident fugiat temporibus facilis
-                        quibusdam? Impedit tenetur possimus praesentium! Culpa libero accusamus id in minima magni quasi
-                        labore sapiente! Sit perferendis, inventore facere consequatur impedit hic, recusandae
-                        voluptatum aperiam qui quis saepe. Quia fugiat perferendis nihil iusto iste molestiae porro
-                        molestias rem exercitationem! Rem recusandae explicabo fugit debitis accusamus tempore!
-                        Mollitia, minima quaerat.</p>
+                    <p>With magnificent views of the of the shimmering ocean and a luxurious bed, all Suites are designed to leave you with an unforgettable experience.
+                        Amenities, World-class butler service and 24-hour concierge,
+                        In-suite check-in and check-out,
+                        Access to the facilities at our iconic spa and The Terrace, and our private beach
+                        Unlimited entry to Summersalt Beach Club and Wild Wadi Waterpark™
+                        Complimentary tour Inside The Pearl
+                        Private living room with a dedicated powder room
+                        A master bathroom complete with a full-size Jacuzzi and a separate five-head rain shower
+                        Hermès amenities
+                        Complimentary wi-fi, a range of 42-inch and 32-inch widescreen interactive HD TV
+                        Remote controlled environment including curtains, TV, music and lights
+                        Macbook pro laptop and all-in-one printers can be provided upon request
+                        Selection of 9 types of pillows, eiderdown duvets, Egyptian cotton linen and a bed personalised for your specific posture preferences.</p>
                 </div>
             </div>
 
             <div id="room">
 
                 <div id="description">
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. At ut vero, id perferendis animi et
-                        enim corrupti, debitis praesentium explicabo atque ex odit? Atque, excepturi. Laudantium
-                        commodi, fugit repudiandae veritatis id provident reprehenderit iusto hic iste blanditiis rem
-                        minima atque asperiores, voluptate fuga similique qui? Quos ut quis numquam dolorem optio minima
-                        quaerat, libero cumque. Cum, repellat. Incidunt eius expedita, animi quisquam ipsam, inventore
-                        tenetur, magni labore possimus repellendus nobis rem illo provident fugiat temporibus facilis
-                        quibusdam? Impedit tenetur possimus praesentium! Culpa libero accusamus id in minima magni quasi
-                        labore sapiente! Sit perferendis, inventore facere consequatur impedit hic, recusandae
-                        voluptatum aperiam qui quis saepe. Quia fugiat perferendis nihil iusto iste molestiae porro
-                        molestias rem exercitationem! Rem recusandae explicabo fugit debitis accusamus tempore!
-                        Mollitia, minima quaerat.</p>
+                    <p>The Pearl Aquarium is a remarkable, one-of-a-kind restaurant, pool, beach and cabana space, offering hotel guests and The Pearl members the finest Arabian hospitality. The private 10,000sqm outdoor luxury leisure facility stretches 100 metres out into the sea and is home to SAL Restaurant and two stunning pools, ideal for relaxing, cooling down and soaking up the sun within plush surroundings.
+
+                        Our freshwater family-friendly pool offers a shaded swim up bar at its centre, setting the perfect tone for relaxing and socialising. Our saltwater infinity pool is an irresistible adult-only escape right above the sea. With four Jacuzzis, picturesque views and a stretch of sand, it makes for a truly memorable experience by the water.</p>
                 </div>
                 <img src="./assets/img/b2df4a23b7c149c8a5895028d1ccffb7.jpeg" alt="">
             </div>
 
             <div id="room">
-                <img src="./assets/img/Restaurant-Cucina-Byblos_Ambiance_Hotel-Byblos-Saint-Tropez-14-1600x1000.jpg"
-                    alt="">
+                <img src="./assets/img/Restaurant-Cucina-Byblos_Ambiance_Hotel-Byblos-Saint-Tropez-14-1600x1000.jpg" alt="">
                 <div id="description">
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. At ut vero, id perferendis animi et
-                        enim corrupti, debitis praesentium explicabo atque ex odit? Atque, excepturi. Laudantium
-                        commodi, fugit repudiandae veritatis id provident reprehenderit iusto hic iste blanditiis rem
-                        minima atque asperiores, voluptate fuga similique qui? Quos ut quis numquam dolorem optio minima
-                        quaerat, libero cumque. Cum, repellat. Incidunt eius expedita, animi quisquam ipsam, inventore
-                        tenetur, magni labore possimus repellendus nobis rem illo provident fugiat temporibus facilis
-                        quibusdam? Impedit tenetur possimus praesentium! Culpa libero accusamus id in minima magni quasi
-                        labore sapiente! Sit perferendis, inventore facere consequatur impedit hic, recusandae
-                        voluptatum aperiam qui quis saepe. Quia fugiat perferendis nihil iusto iste molestiae porro
-                        molestias rem exercitationem! Rem recusandae explicabo fugit debitis accusamus tempore!
-                        Mollitia, minima quaerat.</p>
+                    <p>Birthdays, anniversaries, promotions… or perhaps it’s simply the start of the weekend that calls for a celebration. When a special occasion arises, we have the perfect beachside restaurants for you to truly live the moment. Whether you’ve just arrived in California for a special holiday or you’re a long-time resident celebrating a momentous anniversary, our dedicated teams are always on hand to make it a sensational experience. Explore the best beach restaurants for an unforgettable celebration.</p>
                 </div>
             </div>
 
         </div>
+
+
+
     </main>
     <footer>
+
         <div id="separation"></div>
         <div id="infos">
             <img id="footLogo" src="./assets/img/Thepearl No BG.png" alt="">
             <div>
                 <nav>
-                    <a href=""><li>HOME</li></a>
-                    <a href=""><li>SUITES</li></a>
-                    <a href=""><li>ACTIVITIES</li></a>
-                    <a href=""><li>CONTACT</li></a>
+                    <a href="#top">
+                        <li>HOME</li>
+                    </a>
+                    <a href="./assets/lib/Reservation/reservation.php">
+                        <li>RESERVATION</li>
+                    </a>
+                    <a href="./assets/lib/Contact/contact.php">
+                        <li>CONTACT</li>
+                    </a>
+                    <a href="">
+                        <li>CONNECTION</li>
+                    </a>
                 </nav>
             </div>
             <div>
